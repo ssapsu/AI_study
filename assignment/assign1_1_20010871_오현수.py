00cbc774ce577c053a33c7b5e8092cc819dba306
Hyeonsu Oh <hans324oh@gmail.com>
2022-03-17 10:37:57 +0900

    수정

--- assignment/assign1_1_20010871_오현수.py
+++ assignment/assign1_1_20010871_오현수.py
@@ -1,4 +1,23 @@
-from curses.ascii import isdigit
+'''
+동작 조건: 연산자와 피연산자 사이에 띄어쓰기를 넣어줘야 작동함
+코드 설명: 
+1. 띄어쓰기를 기반으로 연산자와 피연산자를 구별함
+2. 종료 조건 q를 만족시키기 위해 먼저 고려해야 할 대상으로 if elif문의 최상단에 배치
+3. 사칙연산의 로직은 띄어쓰기를 기준으로 입력을 split하여 expressionList에 넣어줌
+4. expressionList의 1번 인덱스의 값을 기준으로 연산의 종류를 파악함
+5. expressionList의 1번 인덱스가 사칙연산 기호 중 한개라면 피연산자가 숫자인지를 판단
+6. 위 조건을 만족하지 않을 경우 사용자에게 입력을 다시 할 것을 요청함
+7. /의 경우 0으로 나누는 케이스를 예외처리하여 런타임에러를 피함
+8. while True로 위 과정을 반복함
+
+'''
+
+def isNumber(s):
+  try:
+    float(s)
+    return True
+  except ValueError:
+    return False
 
 while True:
     print('-'*20+"\n\n\n"+"계산식을 입력하시오. (종료: q)")
@@ -18,34 +37,34 @@ while True:
     
     #* '+'일 때
     if '+' in expressionList[1]:
-        if not (expressionList[0].isdigit() & expressionList[2].isdigit()):
+        if not (isNumber(expressionList[0]) & isNumber(expressionList[2])):
             print("입력을 다시 해주시길 바랍니다.\n\n")
         else:    
-            print(expressionList[0] + expressionList[1] + expressionList[2] + " = " + str(int(expressionList[0]) + int(expressionList[2])) + "\n\n")
+            print(expressionList[0] + " " + expressionList[1] + " " + expressionList[2] + " = " + str(float(expressionList[0]) + float(expressionList[2])) + "\n\n")
         
     #* '*'일 때
     elif '*' in expressionList[1]:
-        if not (expressionList[0].isdigit() & expressionList[2].isdigit()):
+        if not (isNumber(expressionList[0]) & isNumber(expressionList[2])):
             print("입력을 다시 해주시길 바랍니다.\n\n")
         else:    
-            print(expressionList[0] + expressionList[1] + expressionList[2] + " = " + str(int(expressionList[0]) * int(expressionList[2])) + "\n\n")
+            print(expressionList[0] + " " + expressionList[1] + " " + expressionList[2] + " = " + str(float(expressionList[0]) * float(expressionList[2])) + "\n\n")
         
     #* '/'일 때
     elif '/' in expressionList[1]:
-        if not (expressionList[0].isdigit() & expressionList[2].isdigit()):
+        if not (isNumber(expressionList[0]) & isNumber(expressionList[2])):
             print("입력을 다시 해주시길 바랍니다.\n\n")
             #! 0으로 나눌 때 예외처리를 한다.
         elif int(expressionList[2]) == 0:
             print("0으로 나눌 수 없습니다." + "\n\n")
         else:
-            print(expressionList[0] + expressionList[1] + expressionList[2] + " = " + str(int(expressionList[0]) / int(expressionList[2])) + "\n\n")
+            print(expressionList[0] + " " + expressionList[1] + " " + expressionList[2] + " = " + str(float(expressionList[0]) / float(expressionList[2])) + "\n\n")
 
     #* '-'일 때
     elif '-' in expressionList[1]:
-        if not (expressionList[0].isdigit() & expressionList[2].isdigit()):
+        if not (isNumber(expressionList[0]) & isNumber(expressionList[2])):
             print("입력을 다시 해주시길 바랍니다.\n\n")
         else:    
-            print(expressionList[0] + expressionList[1] + expressionList[2] + " = " + str(int(expressionList[0]) - int(expressionList[2])) + "\n\n")
+            print(expressionList[0] + " " + expressionList[1] + " " + expressionList[2] + " = " + str(float(expressionList[0]) - float(expressionList[2])) + "\n\n")
         
     else:
         print("입력을 다시 해주시길 바랍니다.\n\n")
--- assignment/assign1_2_20010871_오현수.py
+++ assignment/assign1_2_20010871_오현수.py
@@ -1,13 +1,20 @@
+'''
+코드 설명: 
+1. 13번 돌아가는 for문에서 문자열을 변수 inputStr에 저장
+2. 문자열의 앞부분의 정보를 저장할 dataList1과 뒷부분의 정보를 저장할 dataList2를 append함수와 문자열 슬라이싱을 활용하여 리스트에 저장
+3. zip함수를 사용하여(list들을 인덱스 순으로 튜플 형식으로 반환해줌; iterable하기에 for문에서 사용 가능) value1과 value2에 누적합을 함
+4. 13으로 나누고 출력하기
+'''
+
 dataList1 = []; dataList2 = []
-value1 = 0
-value2 = 0
+value1 = 0; value2 = 0
 
 for i in range(0, 13):
     inputStr = input()
     dataList1.append(inputStr[:2])
     dataList2.append(inputStr[-2:])
 
-for i, (j, k) in enumerate(zip(dataList1, dataList2), start = 1):
+for (j, k) in zip(dataList1, dataList2):
     value1 += int(j)
     value2 += int(k)
     
